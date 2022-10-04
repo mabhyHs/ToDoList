84ff3b7dfcb384384cb81f4c140c08e5b4be81e2
mabhyhs <m4huens@gmail.com>
2022-10-04 14:14:26 -0300

    first commit

---
+++ src/js/componentes.js
@@ -0,0 +1,38 @@
+import { Todo } from "../classes";
+import { todoList, TodoList } from "../index";
+
+//referencias html
+const divTodoList = document.querySelector(".todo-list");
+const txtInput = document.querySelector(".new-todo");
+
+export const crearTodoHtml = (todo) => {
+  const htmlTodo = `
+    <li class="${todo.completado ? "completed" : ""}" data-id="abc">
+						<div class="view">
+							<input class="toggle" type="checkbox" ${todo.completado ? "checked" : ""}>
+							<label>${todo.tarea}</label>
+							<button class="destroy"></button>
+						</div>
+						<input class="edit" value="Create a TodoMVC template">
+					</li>
+    
+    `;
+  const div = document.createElement("div");
+  div.innerHTML = htmlTodo;
+
+  divTodoList.append(div);
+
+  return div;
+};
+
+//eventos
+txtInput.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13 && txtInput.value.length > 0) {
+    console.log(txtInput.value);
+    const nuevoTodo = new Todo(txtInput.value);
+    todoList.nuevoTodo(nuevoTodo);
+
+    crearTodoHtml(nuevoTodo);
+    txtInput.value = "";
+  }
+});
