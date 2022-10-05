7235e3b5240019248e4b27a1b48cf45a2f4ffdff
mabhyHs <m4huens@gmail.com>
2022-10-05 11:06:59 -0300

    toDo Final

--- src/index.js
+++ src/index.js
@@ -5,10 +5,4 @@ import { crearTodoHtml } from "./js/componentes";
 
 export const todoList = new TodoList();
 
-const tarea = new Todo("aprender js!!!!!");
-
-todoList.nuevoTodo(tarea);
-
-console.log(todoList);
-
-crearTodoHtml(tarea);
+todoList.todos.forEach(crearTodoHtml);
